d7e36eff760ee2bc133ea48f9ad5be3d90a0ffac
Jessica Landeros <jessicalanderos@Jessicas-iMac.local>
2024-05-01 21:27:46 -0700

    WEEK 10 Assignment

--- src/main/java/projects/ProjectsApp.java
+++ src/main/java/projects/ProjectsApp.java
@@ -11,24 +11,43 @@ import projects.service.ProjectService;
 
 
 
-public class ProjectsApp {
-	private Scanner scanner1 = new Scanner(System.in);
+    public class ProjectsApp {
 	private ProjectService projectService = new ProjectService();
 	private Project curProject; 
+	private Scanner scanner = new Scanner(System.in);
+	public static void main (String[]args) {
+			new ProjectsApp().processUserSelections(); 
+	}
 	
-
-
-		public static void main (String[]args) {
-			new ProjectsApp().processUserSelections(); 	
+		private void selectProject() { 
+		listProjects(); 
+		Integer projectId = getIntInput("Enter a project ID to select a project"); 
+		
+		curProject = null;
+		
+		curProject = projectService.fetchProjectById(projectId); 
+		
+	
+		}
+		
+		private void listProjects() {
+			List<Project> projects = projectService.fetchAllProjects(); 
+			
+			System.out.println("\nProjects:");
+			
+			projects.forEach(project -> System.out.println("  " + project.getProjectId() +": " + project.getProjectName())); 
+			
 		}
 		
 		//@formatter:off
 		private List<String> operations = List.of(
-				"1) Add a project"
+				"1) Add a project",
+				"2) List projects",
+				"3) Select a project"
 				);
 		//@formatter:on
 		
-		private Scanner scanner = new Scanner(System.in);
+		
 		
 		private void processUserSelections() {
 			boolean done = false; 
@@ -44,6 +63,15 @@ public class ProjectsApp {
 					case 1: 
 						createProject();
 						break;
+						
+					case 2:
+						listProjects();
+						break; 
+						
+					case 3:
+						selectProject(); 
+						break;
+						
 					
 						
 						default: 
@@ -60,7 +88,7 @@ public class ProjectsApp {
 
 		private boolean exitMenu() {
 			// TODO Auto-generated method stub
-			return false;
+			return true;
 		}
 
 		private int getUserSelection() {
@@ -70,14 +98,22 @@ public class ProjectsApp {
 			return Objects.isNull(input) ? -1 : input; 
 		
 		}
+		
 
 
 		private void printOperations() {
 			System.out.println("\nThese are the available selections. Press the Enter key to quit:"); 
 			operations.forEach(line -> System.out.println(" " + line)); 	
-		}
+	
 		
-		
+			if(Objects.isNull(curProject)) {
+				System.out.println("\nYou are not working with a project."); 
+			}
+			else {
+				System.out.println("\nYou are working with project: " + curProject); 
+			
+			} 
+		} 
 		private Integer getIntInput(String prompt) {
 			String input = getStringInput(prompt); 
 			
--- src/main/java/projects/service/ProjectService.java
+++ src/main/java/projects/service/ProjectService.java
@@ -1,19 +1,34 @@
 package projects.service;
 
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
 import projects.dao.ProjectDao;
 import projects.entity.Project;
 
 public class ProjectService {
-private ProjectDao projectDao = new ProjectDao(); 
+private ProjectDao projectDao = new ProjectDao();
+private Object projectId; 
 	
 	
 public Project addProject(Project project) {
 	return projectDao.insertProject(project); 
 	
 }
-	
-	
-	 
+
+
+
+public List<Project> fetchAllProjects() {
+	return projectDao.fetchAllProjects(); 
 	
 	
 }
+
+	public Project fetchProjectById(Integer projectId) {
+	return projectDao.fetchProjectById(projectId).orElseThrow(() -> new NoSuchElementException(
+			"Project with project ID=" + projectId + " does not exist.")); 
+}
+
+}
+  
